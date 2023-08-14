0d3ea7ef589222428ba37729c6f56258567e392f
elias <eliaslaramartinez@gmail.com>
2023-08-14 17:37:58 -0400

    curso php

--- ejercicio14.php
+++ ejercicio14.php
@@ -1,7 +1,12 @@
 <?php
 
-//Ciclo for
+    //Ciclo for
 
+    for($numeroInicial=0;$numeroInicial<10;$numeroInicial++){
+
+        echo "Numero ".$numeroInicial."<br/>";
+
+    }
 
 
 
---
+++ ejercicio15.php
@@ -0,0 +1,11 @@
+<?php
+
+$numeroInicial =0;
+
+do{
+    echo "Numero ".$numeroInicial."<br/>";
+    $numeroInicial++;
+}while($numeroInicial<10)
+
+
+?>
