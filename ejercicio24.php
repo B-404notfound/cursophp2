26bb92f74e590188a1f5ffb23a4cc0ea5c035b87
elias <eliaslaramartinez@gmail.com>
2023-08-17 13:24:03 -0400

    poo php

--- ejercicio24.php
+++ ejercicio24.php
@@ -1,5 +1,32 @@
 <?php
 
+class Persona{
+    
+    public $nombre; // propiedades
 
+    public $apellido;
+
+    public function asignarNombre($nuevoNombre){  // acciones o metodos 
+        $this->nombre=$nuevoNombre;
+
+    }
+
+    public function asignarApellido($nuevoApellido){
+
+        $this->apellido=$nuevoApellido;
+    }
+
+    
+}
+
+$objAlum = new Persona(); // instancia o creacion de un objeto
+$obj2 = new Persona();
+
+$objAlum->asignarNombre("Elias"); // llamando a un metodo 
+$obj2->asignarApellido("Lara");
+
+echo $obj2->apellido;
+
+echo $objAlum->nombre; // imprimir una propiedad 
 
 ?>
