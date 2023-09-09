dafc7ef695d751b26fad58e0b0e9c0b8899c140c
elias <eliaslaramartinez@gmail.com>
2023-09-09 13:27:30 -0300

    actualizando

--- ejercicio7.php
+++ ejercicio7.php
@@ -15,16 +15,33 @@
         }
 
     }
+    class Instalacion extends Persona{
+        public $nombreInstalacion;
+        public $supervisor;
 
+        public function nombreSupervisor($nsuper){
+                $this->supervisor=$nsuper;
+        }
+    }
+
+
+    $super1 = new Instalacion();
     $pedad1 = new Persona();
     $persona1 = new Persona();
 
+    $guardia1 = new Persona();
+
+    $super1->nombreSupervisor("Alex Apablaza");
+
     $persona1->asignarNombre("Pedro"."<br/>");
 
+    $guardia1->asignarNombre("Felipe");
     $pedad1->asignarEdad(44,"<br/>");
 
     echo $pedad1->edad;
     echo $persona1->nombre;
+    echo $guardia1->nombre."<br/>";
 
+    echo "El nombre del supervisor es : ".$super1->supervisor;
 ?>
 
