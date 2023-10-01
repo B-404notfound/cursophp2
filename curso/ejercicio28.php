60cfd34d40b299a52709110a0dacc65bc57d5858
elias <eliaslaramartinez@gmail.com>
2023-10-01 16:31:22 -0300

    conexion mysql

--- curso/ejercicio28.php
+++ curso/ejercicio28.php
@@ -7,6 +7,12 @@ $password = "";
 try {
     $conexion= new PDO("mysql:host=$server;dbname=album",$user,$password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    
+   $sql="INSERT INTO `fotos` (`id`, `nombre`, `ruta`) VALUES (NULL, 'jugando con mi amigo', 'uso.jpg');";
+
+    $conexion->exec($sql);
+
     echo "Conexión establecida";
 } catch (PDOException $error) {
     echo "Conexión erronea".$error;
