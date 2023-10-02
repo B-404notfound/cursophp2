20b3818db4e76578afbcfe885ba505f12076e538
elias <eliaslaramartinez@gmail.com>
2023-10-02 14:35:26 -0300

    curso php

--- curso/ejercicio30_1.php
+++ curso/ejercicio30_1.php
@@ -2,7 +2,13 @@
 
 session_start();
 
-echo "Usuario: ".$_SESSION["usuario"]." estatus: ".$_SESSION["estatus"];
+if(isset($_SESSION["usuario"])){
+    echo "Usuario: ".$_SESSION["usuario"]." estatus: ".$_SESSION["estatus"];
+}else{
+    echo "No hay datos";
+}
+
+
 
 
 ?>
